5606839b3414490a465eba1973bbf0853b59c2ee
lordKelvin <lord-Kelvin@mail.ru>
2011-04-07 23:47:45 +0400

    core and minimal changes

--- core.py
+++ core.py
@@ -85,14 +85,14 @@ class Core(object):
             rib1 = self.B[i][1] * u[0] - self.B[i][0] * u[1] > self.N[i]
             fail = False
             kmin = 0
-            for j, n2 in enumerate(outline):
-                n1 = outline[j - 1]
+#            for j, n2 in enumerate(outline):
+            for j in xrange(len(outline)):
+#                n1 = outline[j - 1]
                 C = (c[0] - u[0], c[1] - u[1])
-#                B = (n2[0] - n1[0], n2[1] - n1[1])
-                X = (u[0] - n1[0], u[1] - n1[1])
-                div = C[0] * B[1] - C[1] * B[0]
+                X = (u[0] - outline[j - 1][0], u[1] - outline[j - 1][1])
+                div = C[0] * self.B[j - 1][1] - C[1] * self.B[j - 1][0]
                 if div:
-                    k = (B[0] * X[1] - B[1] * X[0]) / float(div)
+                    k = (self.B[j - 1][0] * X[1] - self.B[j - 1][1] * X[0]) / float(div)
                     if k > 0:
                         m = (C[0] * X[1] - C[1] * X[0]) / float(div)
                         if m == 1:
